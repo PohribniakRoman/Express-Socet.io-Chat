f2248c26733b5ac52a1d356be35dc92884cf896f
Roman <roman.pohribniak@gmail.com>
2021-04-18 21:23:43 +0300

    first deploy

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+const server = require("http").createServer(app);
+const io = require("socket.io")(server);
+
+const PORT = process.env.PORT || 3000;
+
+const massages = [];
+
+io.on("connection", (socket) => {
+  socket.on("massage", (data) => {
+    massages.push(data);
+    io.emit("sendMassage", data);
+  });
+});
+
+app.get("/", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "index.html"));
+});
+
+server.listen(PORT, () => {
+  console.log(`Server has been started on ${PORT}...`);
+});
